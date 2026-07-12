82a5401d6b298c913e05049a52bc837676721594
v64844866-crypto <v64844866@gmail.com>
2026-07-12 23:18:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("HSE REPORT PHL");
+
+
+  document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    const empId = document.querySelectorAll("input")[0].value;
+    const password = document.querySelectorAll("input")[1].value;
+
+    // यहाँ आप अपनी मर्ज़ी का ID और पासवर्ड सेट कर सकते हैं
+    if(empId === "0301" && password === "Vishal") {
+        alert("लॉगिन सफल!");
+        // लॉगिन होने के बाद जहाँ भेजना है, उस पेज का नाम नीचे लिखें
+        // window.location.href = "dashboard.html"; 
+    } else {
+        alert("गलत Employee ID या Password!");
+    }
+});
+
+    
+
